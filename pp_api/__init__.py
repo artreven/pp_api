0de83314d2fd6a11537604ca9068d4e7492e1647
artreven <artreven@gmail.com>
2017-11-17 13:34:16 +0100

    modify variable names

--- pp_api/__init__.py
+++ pp_api/__init__.py
@@ -1,3 +1,4 @@
 from pp_api.pp_calls import *
 from pp_api.sparql_calls import *
 from pp_api.gs_calls import *
+import pp_api.utils
--- pp_api/server_data/profit.py
+++ pp_api/server_data/profit.py
@@ -1,6 +1,6 @@
 server = 'http://profit.poolparty.biz'
-pid = '1DE00088-2B4C-0001-9BB3-1C5234FF8640'
-sparql_endpoint = 'https://profit-virtuoso.poolparty.biz/sparql'
+profit_pid = '1DE00088-2B4C-0001-9BB3-1C5234FF8640'
+virtuoso_sparql_endpoint = 'https://profit-virtuoso.poolparty.biz/sparql'
 ld_graphs = {
     'http://www.telegraph.co.uk/business/rss.xml': ['text', 'title', 'title'],
     'http://feeds.bbci.co.uk/news/business/your_money/rss.xml': ['text', 'title', 'title'],
