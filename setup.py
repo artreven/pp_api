082dd34f8c9090f8bcb48b460e8ccc0e80aa205c
Bujaček, Karol <karol.bujacek@eea.sk>
2018-02-15 13:51:33 +0100

    Revert "Fixed reading of requirements from the file"
    This reverts commit 29be75d

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from distutils.core import setup
 
-requirements = []
-
-with open('requirements.txt', 'r') as f:
-    for line in f:
-        requirements.append(str(line.strip()))
-
 setup(
     name='pp_api',
     version='0.1dev',
     packages=['pp_api', 'pp_api.server_data'],
     license='MIT',
-    requires=requirements
+    requires=open('requirements.txt', 'r').read().split()
 )
