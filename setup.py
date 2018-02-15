29be75dcebf5b56fcb0f0fb8914eca09ff0e36be
Bujaček, Karol <karol.bujacek@eea.sk>
2018-02-15 13:26:34 +0100

    Fixed reading of requirements from the file

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from distutils.core import setup
 
+requirements = []
+
+with open('requirements.txt', 'r') as f:
+    for line in f:
+        requirements.append(str(line.strip()))
+
 setup(
     name='pp_api',
     version='0.1dev',
     packages=['pp_api', 'pp_api.server_data'],
     license='MIT',
-    requires=open('requirements.txt', 'r').read().split()
+    requires=requirements
 )
