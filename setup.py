24db71a2ef75bfa2885509f88f3e145e1f206a97
Peter Hanecak <hanecak@opendata.sk>
2018-03-02 09:08:02 +0100

    restored version 0.1dev for develop branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pp_api',
-    version='profit-v11',
+    version='0.1dev',
     packages=['pp_api', 'pp_api.server_data'],
     license='MIT',
     requires=open('requirements.txt', 'r').read().split()
