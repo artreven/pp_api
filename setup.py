b8aa338907cd09fe8c50e6f15a550d1802eed980
Peter Hanecak <hanecak@opendata.sk>
2018-03-02 09:10:56 +0100

    pp_api PROFIT release v11.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pp_api',
-    version='0.1dev',
+    version='profit-v11.1',
     packages=['pp_api'],
     license='MIT',
     requires=open('requirements.txt', 'r').read().split()
