d816757638797f0fb713d9065d24a32b5360253d
mireleschavezv <mireleschavezv@semantic-web.com>
2018-06-20 16:54:51 +0200

    moved to setup tools

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+
+requirements = open('requirements.txt', 'r').read().split("\n")
+requirements = [x for x in requirements if ((len(x) > 0)
+                                            and (x[0] != '-')
+                                            and ("+" not in x))]
+requirements = [x.replace("python-", "python_") for x in requirements]
+dependencies = ["https://github.com/semantic-web-company/nif/tarball/master#egg=nif"]
 
 setup(
     name='pp_api',
     version='0.1dev',
+    description='Library for accessing PoolParty APIs',
     packages=['pp_api'],
     license='MIT',
-    requires=open('requirements.txt', 'r').read().split()
+    install_requires=requirements,
+    dependency_links=dependencies
 )
