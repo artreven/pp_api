d0f9f821babb7783c9008b86ad5a3af19c881428
artreven <artreven@gmail.com>
2018-06-20 16:42:58 +0200

    modify setup

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='pp_api',
     version='0.1dev',
     packages=['pp_api'],
     license='MIT',
-    requires=open('requirements.txt', 'r').read().split()
+    requires=requirements
 )
