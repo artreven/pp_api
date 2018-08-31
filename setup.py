e57abaff67beb38e9700676094d341ceb29377d3
Peter Hanecak <hanecak@opendata.sk>
2018-08-31 14:20:49 +0200

    PROFIT release 17

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dependencies = ["https://github.com/semantic-web-company/nif/tarball/master#egg=
 
 setup(
     name='pp_api',
-    version='profit-v16',
+    version='profit-v17',
     description='Library for accessing PoolParty APIs',
     packages=['pp_api'],
     license='MIT',
